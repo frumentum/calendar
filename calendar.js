0374f129eb9def13c2edecd10efc9214a763c58c
frumentum <frumentum@posteo.de>
2018-09-15 10:20:13 +0200

    sortedOrder is debugged: Now sortedOrder is the result from comparing startTime[i] and startTime[i+1] or timeSpan[i] and timeSpan[i+1] respectively. That's done in createSortedOrder()

--- calendar.js
+++ calendar.js
@@ -8,6 +8,7 @@ addDate('14:30', '15:00', allDates)
 addDate('15:30', '16:00', allDates)
 // sort the array by the timespan: the greater timespans come first!
 addInfoAboutSorting(allDates)
+createSortedNumbers(allDates)
 console.log(allDates)
 
 function addDate (startTime, endTime, allDatesArray) {
@@ -57,20 +58,60 @@ function addInfoAboutSorting (allDatesArray) {
     allDatesArray.forEach(obj => {
       obj.sortByTimeSpan = uniqueTimeSpan.indexOf(obj.timeSpan)
     })
+  }
+}
 
-    // add the sorting numbers to get the 'real' order we can use later!
-    // to reach this use the same procedure as above
-    allDatesArray.forEach(obj => {
-      obj.sortedOrder = obj.sortByTimeSpan + obj.sortByStartTime
-    })
-    let sortedOrder = []
-    allDatesArray.forEach(obj => sortedOrder.push(obj.sortedOrder))
-    const uniqueSortedOrder = [...new Set(sortedOrder)]
-    uniqueSortedOrder.sort((a, b) => a - b)
-    allDatesArray.forEach(obj => {
-      // overwrite the sorted order by their ordered index
-      obj.sortedOrder = uniqueSortedOrder.indexOf(obj.sortedOrder)
-    })
+// Create sorted numbers to get the 'real' order we can use later to define the
+// right places for each event/date/box.
+function createSortedNumbers (allDatesArray) {
+  let startTime = []
+  allDatesArray.forEach(obj => startTime.push(obj.sortByStartTime))
+  let timeSpan = []
+  allDatesArray.forEach(obj => timeSpan.push(obj.sortByTimeSpan))
+  for (let i = 0; i < startTime.length; i++) {
+    // last case at first: if i is startTime.length - 1 , startTime[i + 1]
+    // doesn't exist. In this case look at the last sortedOrder and check its
+    // number
+    if (i === startTime.length - 1) {
+      const compare1 = allDatesArray[i - 1].sortedOrder
+      const compare2 = allDatesArray[i - 1].sortByStartTime
+      // if the last sortedOrder was the result of starTime, this sortedOrder
+      // would be the result of startTime, too. Otherwise it results from
+      // time span!
+      if (compare1 === startTime.indexOf(compare2)) {
+        allDatesArray[i].sortedOrder = startTime.indexOf(startTime[i])
+      } else {
+        allDatesArray[i].sortedOrder = timeSpan.indexOf(timeSpan[i])
+      }
+      // at least sort the objects of allDatesArray by its sortedOrder
+      allDatesArray.sort((a, b) => a.sortedOrder - b.sortedOrder)
+      break
+    }
+    let compare1 = startTime[i]
+    let compare2 = startTime[i + 1]
+    if (compare1 < compare2) {
+      allDatesArray[i].sortedOrder = startTime.indexOf(compare1)
+      continue
+    }
+    if (compare1 > compare2) {
+      // Because allDatesArray's objects are ordered by start time, this case
+      // should not occure!
+      console.log(`An error occured: startTime[${i}] > startTime[${i + 1}]`)
+      break
+    }
+    // if we get here, startTime[i] and startTime[i + 1] will be identically.
+    // So let's see, if their time spans differ!
+    compare1 = timeSpan[i]
+    compare2 = timeSpan[i + 1]
+    if (compare1 <= compare2) {
+      // If the first event takes longer or is identically to the second one,
+      // use the index of the first one to set sortedOrder.
+      allDatesArray[i].sortedOrder = timeSpan.indexOf(compare1)
+      continue
+    } else {
+      // Otherwise use the second event!
+      allDatesArray[i].sortedOrder = timeSpan.indexOf(compare2)
+    }
   }
 }
 
